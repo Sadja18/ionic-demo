6f0bc473e9e5fd59c2108954c89235a356c48bc4
NAMAN MISHRA <namanm@egov.cdac.in>
2025-01-14 16:59:41 +0530

    PDF Gen Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'profile-list',
+    redirectTo: 'export-profile',
     pathMatch: 'full'
   },
   {
